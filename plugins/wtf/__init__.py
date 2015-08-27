627bd0f6568b5ccddd3bf822728fce7e52772f4e
tehron <tehron@quantentunnel.de>
2015-08-27 18:55:50 +0200

    add nr of definitions to output

--- plugins/wtf/__init__.py
+++ plugins/wtf/__init__.py
@@ -41,14 +41,30 @@ def wtf(connection, channel, nick, cmd, args):
     if index >= len(entries) or index < 0:
         return plugins.say(connection, channel, "No definition nr %d available" % (index + 1))
 
+    count = "?"
+    count_div = tree.xpath("//div[contains(@class, 'definition-count-panel')]")
+    if count_div:
+        try:
+            count = str(1 + int(count_div[0].text_content().split()[0]))
+        except Exception as e:
+            print e
+            pass
+
+    txt = "\x02[Definition 1/%s]\x0f " % count
     definition = extract_text(entries[index], ".//div[@class='meaning']")
-    if len(definition) > 450:
-        definition = definition[:450] + "..."
+    if len(definition) > 300:
+        definition = definition[:300] + "..."
+    txt += definition
+    
     plugins.say(connection, channel, definition)
+
     example = extract_text(entries[index], ".//div[@class='example']")
     if example:
-        if len(example) > 440:
-            example = example[:440] + "..."
-        plugins.say(connection, channel, "\x02Example:\x0f " + example)
+        if len(example) > 300:
+            example = example[:300] + "..."
+        txt += "\n\x02Example:\x0f " + example
+
+    plugins.say(connection, channel, txt)
 
 plugins.register_pub_cmd("wtf", wtf)
+plugins.register_pub_cmd("define", wtf)
