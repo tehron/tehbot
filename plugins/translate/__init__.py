d1a7a56c1480d9d17a8ddef359787bba2d05aed8
tehron <tehron@quantentunnel.de>
2015-09-05 10:28:02 +0200

    fix encoding

--- plugins/translate/__init__.py
+++ plugins/translate/__init__.py
@@ -8,7 +8,7 @@ def translate(connection, channel, nick, cmd, args):
         return
 
     headers = { 'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64; rv:14.0) Gecko/20100101 Firefox/14.0.1' }
-    data = { 'sl' : 'auto', 'tl' : 'en', 'hl' : 'en', 'ie' : 'UTF-8', 'q' : args }
+    data = { 'sl' : 'auto', 'tl' : 'en', 'hl' : 'en', 'ie' : 'UTF-8', 'q' : plugins.to_utf8(args) }
     req = urllib2.Request("https://translate.google.com/", urllib.urlencode(data), headers)
     tree = lxml.html.parse(urllib2.urlopen(req))
 
