bbec993dcd327971326c1e7ac9d01a333afe12e3
tehron <tehron@quantentunnel.de>
2018-01-29 20:00:08 +0100

    add challenge site wixxerd.com

---
+++ tehbot/plugins/challenge/wix.py
@@ -0,0 +1,32 @@
+import tehbot.plugins as plugins
+import tehbot.settings as settings
+import urllib
+import urllib2
+import urlparse
+import lxml.html
+import re
+
+prefix = "\x0303[wixxerd.com]\x03 "
+url = "http://www.wixxerd.com/challenges/userScore.cfm?username=%s&authkey=%s"
+
+def stats(user, rank):
+    if rank:
+        return "Not implemented yet :/"
+
+    return userstats(user)
+
+def userstats(user):
+    try:
+        page = urllib.urlopen(url % (urllib.quote_plus(user), settings.wixxerd_api_key)).read()
+    except:
+        return "Network error"
+
+    match = page.split(":")
+    if len(match) != 6:
+        return prefix + "The requested user was not found, you can register at http://www.wixxerd.com"
+
+    score, score_total, users_total, challs_solved, challs_total, rank = map(int, match)
+    return prefix + "%s solved %d (of %d) challenges and is on rank %d (of %d)." % (user, challs_solved, challs_total, rank, users_total)
+
+def solvers(challenge_name_or_nr, user=None):
+    return "Not implemented yet :/"
