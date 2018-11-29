4640bd773ef5f5272f24836833375797e4741a77
tehron <tehron@quantentunnel.de>
2018-11-29 20:38:19 +0100

    AiHandler: add more "ai"

--- tehbot/plugins/ai/__init__.py
+++ tehbot/plugins/ai/__init__.py
@@ -1,17 +1,44 @@
 from tehbot.plugins import *
 import tehbot.plugins as plugins
+import wolframalpha
 import re
 import pipes
+import random
 
 class AiHandler(ChannelHandler):
+    @staticmethod
+    def decision_answers():
+        return [
+                "Yes",
+                "No",
+                "Sure",
+                "Absolutely",
+                "For sure",
+                "No, you are!",
+                "Will you ever get sick of this?",
+                "Certainly not",
+                "I haven't made up my mind yet."
+                ]
+
+    def initialize(self, dbconn):
+        ChannelHandler.initialize(self, dbconn)
+        try:
+            self.client = wolframalpha.Client(self.settings["wolframalpha_app_id"])
+            self.ai_enabled = True
+        except:
+            self.ai_enabled = False
+
     def execute(self, connection, event, extra, dbconn):
         botname = self.tehbot.settings.value("botname", connection)
+        decide_regex = [
+                re.compile(r'''^(?:ok(?:ay)?|hey)\s+%s,?\s*(?:is|are|has|was|were)\s+\w[\s\w]*\?''' % botname, re.I),
+                ]
         regex = [
-                re.compile(r'''^ok(?:ay)?\s+%s,?\s*(?P<what>.*?)\s*\??$''' % botname, re.I),
+                re.compile(r'''^(?:ok(?:ay)?|hey)\s+%s,?\s*(?P<what>.*?)\s*\??$''' % botname, re.I),
                 ]
         solved_regex = [
-                re.compile(r'''^ok(?:ay)?\s+%s,?\s*has\s+(?P<who>\w+)\s+solved\s+(?P<chall>\w[\s\w]*?|"[^"]+"|'[^']+')(?:\s+on\s+(?P<site>\w[\s\w]*?|"[^"]+"|'[^']+'))?\s*\??$''' % botname, re.I),
-                re.compile(r'''^ok(?:ay)?\s+%s,?\s*did\s+(?P<who>\w+)\s+solve\s+(?P<chall>\w[\s\w]*?|"[^"]+"|'[^']+')(?:\s+on\s+(?P<site>\w[\s\w]*?|"[^"]+"|'[^']+'))?\s*\??$''' % botname, re.I)
+                re.compile(r'''^(?:ok(?:ay)?|hey)\s+%s,?\s*has\s+(?P<who>\w+)\s+solved\s+(?P<chall>\w[\s\w]*?|"[^"]+"|'[^']+')(?:\s+on\s+(?P<site>\w[\s\w]*?|"[^"]+"|'[^']+'))?\s*\??$''' % botname, re.I),
+                re.compile(r'''^(?:ok(?:ay)?|hey)\s+%s,?\s*did\s+(?P<who>\w+)\s+solve\s+(?P<chall>\w[\s\w]*?|"[^"]+"|'[^']+')(?:\s+on\s+(?P<site>\w[\s\w]*?|"[^"]+"|'[^']+'))?\s*\??$''' % botname, re.I)
                 ]
 
         for r in solved_regex:
@@ -30,13 +57,32 @@ class AiHandler(ChannelHandler):
                 plugin.handle(connection, event, {"args":args}, dbconn)
                 return
 
+        if not self.ai_enabled:
+            return
+
+        for r in decide_regex:
+            match = r.search(extra["msg"])
+            if match is not None:
+                return random.choice(AiHandler.decision_answers())
+
         for r in regex:
             match = r.search(extra["msg"])
             if match is not None:
                 what = match.group(1)
 
-                plugin = self.tehbot.cmd_handlers["ddg"]
-                plugin.handle(connection, event, {"args":what}, dbconn)
-                return
+                try:
+                    for p in self.client.query(what).pods:
+                        if p.id == "Result" and p.text:
+                            txt = " ".join(p.text.splitlines())
+                            return plugins.shorten("%s: %s" % (event.source.nick, txt), 350)
+
+                    for p in self.client.query(what).pods:
+                        if p.id == "Misc" and p.text:
+                            txt = " ".join(p.text.splitlines())
+                            print txt
+
+                    raise Exception("hm")
+                except:
+                    return [("me", "shrugs")]
 
 register_channel_handler(AiHandler())
