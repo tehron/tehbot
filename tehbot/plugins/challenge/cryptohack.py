471fc6fa04e7d8a0e186b44c748c06ad905e7f5b
tehron <tehron@quantentunnel.de>
2020-04-14 19:41:14 +0200

    module challenge: fix cryptohack for unknown users

--- tehbot/plugins/challenge/cryptohack.py
+++ tehbot/plugins/challenge/cryptohack.py
@@ -19,7 +19,7 @@ class Site(BaseSite):
         url = "https://cryptohack.org/wechall/userscore/?username=%s&authkey=%s"
         authkey = self.settings["cryptohack_api_key"]
         html = urllib2.urlopen(url % (Plugin.to_utf8(user), authkey), timeout=5).read()
-        if html == "":
+        if html == "failed":
             return None
         user, rank, score, scoremax, challs_solved, challs_total, users_total = html.split(":")
         return user, str(int(challs_solved)), int(challs_total), str(int(rank)), int(users_total), int(score), int(scoremax), None
