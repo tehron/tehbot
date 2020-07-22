d89ef5022381c4e5ffbf0b97b580f1d87af768e5
tehron <tehron@quantentunnel.de>
2020-07-22 20:40:19 +0200

    module hs: convert dbconn usage

--- tehbot/plugins/hs.py
+++ tehbot/plugins/hs.py
@@ -2,17 +2,25 @@
 from tehbot.plugins import *
 import urllib
 import urllib2
-import time
 import ssl
 import lxml.html
 import re
+from pony.orm import *
+from datetime import datetime, timedelta
 
-class HsSolvedPoller(Poller):
-    def initialize(self):
-        Poller.initialize(self, dbconn)
-        with dbconn:
-            dbconn.execute("create table if not exists HsPollerLastSolves(id integer, ts datetime, user text, challenge text)")
-            dbconn.execute("create table if not exists HsPollerForum(id integer, ts datetime, title text, url text, who text)")
+class HsPoller(Poller):
+    @db_session
+    def create_entities(self):
+        class HsPollerLastSolves(self.db.Entity):
+            ts = Required(datetime)
+            user = Required(str)
+            challenge = Required(str)
+
+        class HsPollerForum(self.db.Entity):
+            ts = Required(datetime)
+            title = Required(str)
+            url = Required(str)
+            who = Required(str)
 
     def execute(self, connection, event, extra):
         url = "https://www.happy-security.de/index.php?modul=hacking-zone"
@@ -20,7 +28,7 @@ class HsSolvedPoller(Poller):
         try:
             reply = urllib2.urlopen(url, timeout=5)
         except (urllib2.URLError, ssl.SSLError):
-            # ignore stupid SSL errors for RevEl
+            # ignore stupid SSL errors
             return
 
         tree = lxml.html.parse(reply)
@@ -29,19 +37,18 @@ class HsSolvedPoller(Poller):
         for si in tree.xpath("//table[@class='mtable']/tr/td[2]"):
             match = re.search(ur'(.*?) eingesendet von .* zuletzt gelöst von (.*?) am (\d+\.\d+\.\d+ \d+:\d+) Uhr', " ".join(si.text_content().split()))
             if match is not None:
-                ts = time.mktime(time.strptime(match.group(3).strip(), "%d.%m.%Y %H:%M"))
+                ts = datetime.strptime(match.group(3).strip(), "%d.%m.%Y %H:%M")
                 who = match.group(2).strip()
                 chall = match.group(1).strip()
                 last_solves.append((ts, who, chall))
 
         msgs = []
-        with dbconn:
-            for ls in last_solves:
-                c = dbconn.execute("select 1 from HsPollerLastSolves where ts=? and user=? and challenge=?", ls)
-                if not c.fetchone():
-                    dbconn.execute("insert into HsPollerLastSolves values(null, ?, ?, ?)", ls)
-                    if time.time() - ls[0] < 86400:
-                        msgs.append(Plugin.green("[Happy-Security Solutions]") + " %s has just solved %s." % (Plugin.bold(ls[1]), Plugin.bold(ls[2])))
+        with db_session:
+            for ts, who, chall in last_solves:
+                if not select(s for s in self.db.HsPollerLastSolves if s.ts == ts and s.user == who and s.challenge == chall):
+                    self.db.HsPollerLastSolves(ts=ts, user=who, challenge=chall)
+                    if datetime.now() - ts < timedelta(days=2):
+                        msgs.append(Plugin.green("[Happy-Security Solutions]") + " %s has just solved %s." % (Plugin.bold(who), Plugin.bold(chall)))
 
         msg = u"\n".join(msgs)
         if msg:
